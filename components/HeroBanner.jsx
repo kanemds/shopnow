040a72f60bbbc2ca6c62e57c382a5db6df8ce3f7
Kanem <kanemdeng@email.com>
2022-07-17 17:34:17 -0700

    connect sanity backend data to front-page

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Link from "next/link"
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client'
+
+const HeroBanner = ( { heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
+        <p className='beats-solo'>{heroBanner.smallText}</p>
       </div>
-      <h3>MID TEXT</h3>
-      <img src="" alt="headphones" className='hero-banner-image' />
+      <h3>{heroBanner.midText}</h3>
+      <h1>{heroBanner.largeText1}</h1>
+      <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image' />
       <div>
-        <Link href="/product/ID">
-            <button type='button'>BUTTON TEXT</button>
+        <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
         </Link>
         <div className='desc'>
           <h5>DESCRIPTION</h5>
-          <p>DESCRIPTION</p>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>
