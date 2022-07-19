481ce608bd204752c54a8eb88d8883d1bfae09de
Kanem <kanemdeng@email.com>
2022-07-18 18:35:19 -0700

    item page quantites can also apply on the cart page

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,10 +5,33 @@ const Context = createContext()
 
 export const StateContext = ({ children }) => {
   const [ showCart, setShowCart ] = useState(false)
-  const [ cartItems, setCartItems ] = useState()
+  const [ cartItems, setCartItems ] = useState([])
   const [ totalPrice, setTotalPrice ] = useState()
-  const [ totalQuantities, setTotalQuantities ] = useState()
+  const [ totalQuantities, setTotalQuantities ] = useState(0)
   const [ qty, setQty ] = useState(1)
+
+  const onAdd = ( product, quantity ) => {
+    const checkProductInCart = cartItems.find(item => item._id === product._id)
+
+    setTotalPrice( prevTotalPrice => prevTotalPrice + product.price * quantity )
+    setTotalQuantities( prevTotalQuantities => prevTotalQuantities + quantity )
+
+    if ( checkProductInCart ) {
+      const updateCartItems = cartItems.map( cartProduct => {
+        if ( cartProduct._id === product._id ) return {
+          ...cartProduct,
+          quantity: cartProduct.quantity + quantity
+        } 
+      })
+      setCartItems(updateCartItems)
+      
+    } else {
+      product.quantity = quantity
+
+      setCartItems([...cartItems, { ...product }])
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`)
+  }
 
   const incQty = () => {
     setQty((prevQty)=> prevQty + 1 )
@@ -24,12 +47,14 @@ export const StateContext = ({ children }) => {
     <Context.Provider 
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
         qty,
         incQty,
-        decQty
+        decQty,
+        onAdd
       }}
     >
       {children}
